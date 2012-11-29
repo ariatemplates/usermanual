abb9cca071193d593652f2778cc8c0889593c715
bcharbonnier <benoit.charbonnier@amadeus.com>
2012-11-29 17:38:56 +0100

    Build improvment to be really sync-async

--- build.js
+++ build.js
@@ -1,16 +1,17 @@
-require("colors");
+﻿require("colors");
 
 var argv = process.argv,
     fs = require('fs'),
-    panda = require('panda-docs');
+    panda = require('panda-docs'),
+    async = require('async');
 
 argv.shift();
 argv.shift();
 
-var version = argv[0];                                                  // Takes the doc version param if provided
-var versions = [];                                                      // Array to store all the versions
+var version = argv[0];     // Takes the doc version param if provided
+var versions = [];         // Array to store all the versions
 
-console.log("GENERATING DOCUMENTATION".green);
+console.log("GENERATING DOCUMENTATION".cyan.bold);
 
 // Check if I have to build all the versions or just one (x.y.z or latest)
 fs.readdir("./src", function(err, files) {
@@ -26,17 +27,29 @@ fs.readdir("./src", function(err, files) {
             return (value === '' || value == "index.md" || value.match(/^\./)) ? 0 : 1;
         });
     }
+    
+    copyFileSync("./resources/documentation.css", "./out/documentation.css");
 
     // For each version generates the html files
+    var series = []
     versions.forEach(function(verj) {
-        makeDevDocs(verj);
+        series.push(function(cb) {
+            console.log("BUILDING ".green + verj.green);
+            makeDevDocs(verj, cb);
+        });
+    });
+    
+    async.series(series, function(err, results) {
+        if (!err) {
+            console.log("DOCUMENTATION GENERATED ✔".green);
+        } else {
+            console.log("GENERATION FAILED ⨯".red.bold);
+        }
     });
 });
 
 // Generates the html files for the documentation
-function makeDevDocs(verj) {
-    
-    copyFileSync("./resources/documentation.css", "./out/documentation.css");
+function makeDevDocs(verj, callback) {
 
     var options = {
         title: "Aria Templates Usermanual (" + verj + ")",
@@ -44,15 +57,19 @@ function makeDevDocs(verj) {
         assets: "./resources/assets",
         disableTests: true,
         output: "./out/" + verj,
-        outputAssets: "./out/" + verj
+        outputAssets: "./out/",
+        extension: ".md"
     }
     
     panda.make(["./src/" + verj], options, function(err, stats) {
-        console.log(stats.files.length + ' files generated');
-        console.log("Done.".bold)
         if (err) {
             console.error(err);
-            process.exit(-1);
+            console.log("BUILD ⨯".red);
+            callback(err);
+        } else {
+            console.log(stats.files.length + ' files generated');
+            console.log("BUILD ✔".green);
+            callback(null)
         }
     });
 }
